0cd211b5461e5c6388db4df451e60f8b2011da48
OleksandraMedvedenko <o.medvedenko21@gmail.com>
2023-08-15 17:13:57 +0300

    On branch main 	new file:   arrow_down.svg 	new file:   arrow_up.svg 	new file:   banner.jpg 	modified:   css/style.css 	modified:   index.html 	modified:   js/script.js

--- js/script.js
+++ js/script.js
@@ -1,20 +1,49 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const subMenuLinks = document.querySelectorAll(".submenu__list-link");
-    
-    subMenuLinks.forEach((link) => {
-        link.addEventListener("click", function (event) {
-            event.preventDefault(); // Отменить переход по ссылке
-            const subMenu = this.parentElement.querySelector(".link-sub");
+    const toggleIcons = document.querySelectorAll(".toggle-icon");
+
+    toggleIcons.forEach((icon) => {
+        icon.addEventListener("click", function () {
+            const subMenu = this.parentElement.querySelector(".submenu");
+            const menuItem = this.parentElement; // Получаем родительский элемент (li.menu__list-item)
+            
             subMenu.classList.toggle("active");
+            menuItem.classList.toggle("active"); // Добавляем/удаляем класс active к пункту меню
+
+            // Добавьте код для изменения стилей шапки и изображения иконки
+            const header = document.querySelector(".header");
+            const logoImg = document.querySelector(".logo__img");
+            const menuListLinks = document.querySelectorAll(".menu__list-link");
+            const submenuListLinks = document.querySelectorAll(".submenu__list-link");
+            const toggleIconImg = this.querySelector("img"); // Получаем img внутри toggle-icon
+
+            if (subMenu.classList.contains("active")) {
+                header.style.backgroundColor = "#fff";
+                menuListLinks.forEach(link => link.style.color = "#101112");
+                submenuListLinks.forEach(link => link.style.color = "#101112");
+                logoImg.style.filter = "invert(1)";
+                toggleIconImg.src = "arrow_up.svg"; // Заменяем изображение
+                
+                // Изменяем цвет элементов в блоке .menu__options на черный
+                const menuOptions = document.querySelector(".menu__options");
+                menuOptions.style.filter = "invert(1)"; // Черный цвет текста
+            } else {
+                //header.style.backgroundColor = "#151515";
+                menuListLinks.forEach(link => link.style.color = "#fff");
+                submenuListLinks.forEach(link => link.style.color = "#101112");
+                logoImg.style.filter = "invert(0)";
+                toggleIconImg.src = "arrow_down.svg"; // Возвращаем исходное изображение
+                
+                // Изменяем цвет элементов в блоке .menu__options на белый
+                const menuOptions = document.querySelector(".menu__options");
+                menuOptions.style.filter = "invert(0)"; // Белый цвет текста
+            }
         });
     });
-    
-    const subMenus = document.querySelectorAll(".link-sub");
-    subMenus.forEach((subMenu) => {
-        const maxWidth = Array.from(subMenu.children).reduce((maxWidth, child) => {
-            return Math.max(maxWidth, child.offsetWidth);
-        }, 0);
-        subMenu.style.width = maxWidth + "px";
-    });
 });
+
+
+
+
+
+
 
